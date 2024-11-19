e10da65e5bdb41d76cf4ac633c0396180f8903ad
ilya <ponoia@yandex.ru>
2024-11-19 19:28:26 +0300

    to-check-version

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 MAX_SIZE = 9
 START_POINTS = 10
+STRING = 3
+COLUMN = 3
+
 
 items = {
     'r': {'size': 3, 'live_points':25},
@@ -15,6 +18,19 @@ items = {
     's': {'size': 2, 'live_points':20},
     'c': {'size': 2, 'live_points':20},
 }
+
+marks = list(items.keys())
+
+def live_points_sign_check(items, points):
+    all_live_points = 0
+    for i, (_,value) in enumerate(items.items()):
+        all_live_points += value['live_points']
+    points += START_POINTS
+    if all_live_points - points < points:
+        return True
+    else:
+        return False
+
 
 def gen_tab(items, max_size=MAX_SIZE):
     table = [[0 for c in range(max_size)] for _ in range(len(items))]
@@ -34,7 +50,11 @@ def gen_tab(items, max_size=MAX_SIZE):
                 if size > limit_size:
                     table[i][col] = pre_live_points
                 else:
-                    used = 0 if col < size else table[i-1][col - size]
+                    if col < size:
+                        used = 0 
+                    else:
+                        used = table[i-1][col - size]
+
                     new_live_points = used + live_points
 
                     res = max(new_live_points, pre_live_points)
@@ -42,8 +62,57 @@ def gen_tab(items, max_size=MAX_SIZE):
     
     return table
 
-if __name__ == '__main__':
-    marks = ['r', 'p', 'a', 'm', 'i', 'k', 'x', 't', 'f', 'd', 's', 'c']
-    table = gen_tab(items)
+
+def gen_inventory(marks, item_size_list, complect):
+    key_value_list = []
+    for i in range(len(marks)):
+        if marks[i] in complect:
+            key_value_list.append((marks[i], item_size_list[i]))
+
+    key_value_list = sorted(key_value_list, key=lambda x: x[1], reverse=True)
+    table = [[0 for _ in range(STRING)] for _ in range(COLUMN)]
+    s = 0 #strings
+    for i in range(len(key_value_list)):
+        c = 0 #columns
+        count = key_value_list[i][1]
+        while count > 0:
+            if table[s][c] == 0:
+                table[s][c] = key_value_list[i][0]
+                count -= 1
+                c += 1
+            else:
+                c += 1
+        if s + 1 < STRING:
+            s += 1
+        else:
+            s = 0
+
     for i in range(len(table)):
-        print(marks[i], table[i])
+        print(table[i])
+
+
+def item_complect(table, items):
+    item_size = []
+    for i, (_, value) in enumerate(items.items()):
+            size = value['size']
+            item_size.append(size)
+
+    avaibale_size = MAX_SIZE - 1
+    complect = []
+    for k in range(len(table) - 1, -1, -1):
+        if avaibale_size - item_size[k] > -2:
+            if table[k][avaibale_size] != table[k - 1][avaibale_size]:
+                complect.append(marks[k])
+                avaibale_size = avaibale_size - item_size[k]
+
+    
+
+    if live_points_sign_check(items, table[len(table) - 1][MAX_SIZE - 1]):
+        gen_inventory(marks, item_size, complect)
+    else:
+        return 'Error'
+
+
+if __name__ == '__main__':
+    table = gen_tab(items)
+    item_complect(table, items)
